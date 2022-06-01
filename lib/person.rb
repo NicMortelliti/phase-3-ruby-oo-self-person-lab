7697464bcdd48307408bf3d75cc952c034d5d649
NicMortelliti <npmortelliti@gmail.com>
2022-06-01 10:28:02 -0700

    Assignment complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,82 @@
-# your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(value)
+    if value > 10 
+      @happiness = 10
+    elsif value < 0
+      @happiness = 0
+    else
+      @happiness = value
+    end
+  end
+
+  def hygiene=(value)
+    if value > 10
+      @hygiene = 10
+    elsif value < 0
+      @hygiene = 0
+    else
+      @hygiene = value
+    end
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    [self, friend].map do |person|
+      person.happiness += 3
+    end
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    people = [self, friend]
+    if topic == "politics"
+      people.map do |person|
+        person.happiness -= 2
+      end
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      people.map do |person|
+        person.happiness += 1
+      end
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+end
+
